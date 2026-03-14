acd9e6999ed5916635ab79d358a46cefa5a77afd
Mathew Coburn <mathew.coburn@gmail.com>
2026-03-14 15:22:30 +0000

    controls updated gravity removed

--- player.gd
+++ player.gd
@@ -1,39 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -800.0
+const SPEED = 30000.0
 const MAX_SPEED = 1000.0
 const FRICTION = 1000.0
 
 signal hit
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("move_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-	if Input.is_action_pressed("move_right") and velocity.x < 0:
-		velocity.x += 2000 * delta
+	if direction.length() > 0:
 		$AnimatedSprite2D.play()
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 500 * delta
-		$AnimatedSprite2D.play()
-	elif Input.is_action_pressed("move_left") and velocity.x > 0:
-		velocity.x -= 2000 * delta
-		$AnimatedSprite2D.play()
-	elif Input.is_action_pressed("move_left"):
-		velocity.x -= 500 * delta
-		$AnimatedSprite2D.play()
+		direction = direction.normalized()
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		$AnimatedSprite2D.stop()
+		
 
-	velocity = velocity.limit_length(MAX_SPEED)
+	velocity = direction * SPEED * delta
 
 	move_and_slide()
 
