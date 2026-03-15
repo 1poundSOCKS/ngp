0bf8c9a0f3989b08d7808438f4a2acee1438970c
Mathew Coburn <mathew.coburn@gmail.com>
2026-03-15 17:11:18 +0000

    bullets!

--- player.gd
+++ player.gd
@@ -5,18 +5,27 @@ const SPEED = 30000.0
 const MAX_SPEED = 1000.0
 const FRICTION = 1000.0
 
-signal hit
+
+#signal hit
+
+var bulletScene : Resource = preload("res://bullet.tscn")
 
 func _physics_process(delta: float) -> void:
 
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction : Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
 	if direction.length() > 0:
 		$AnimatedSprite2D.play()
 		direction = direction.normalized()
 	else:
 		$AnimatedSprite2D.stop()
-		
+	
+	if Input.is_action_just_pressed("shoot"):
+		var bullet : Area2D = bulletScene.instantiate()
+		bullet.position = position + Vector2(0,-100)
+		bullet.velocity = Vector2(0,-1000)
+		get_parent().add_child(bullet)
+		#get_parent().move_child(bullet, 0)
 
 	velocity = direction * SPEED * delta
 
