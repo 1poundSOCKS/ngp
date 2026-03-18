4f2ef75e92592792536a63129aaabb38134d7ff2
Mathew Coburn <mathew.coburn@gmail.com>
2026-03-18 14:40:43 +0000

    new enemies spawn

--- level.gd
+++ level.gd
@@ -4,6 +4,8 @@ extends Node2D
 var scrollPosition : float = 100
 const SCROLL_SPEED : float = 100
 
+const ENEMY_SCENE = preload("res://enemy.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,3 +16,15 @@ func _process(delta: float) -> void:
 	var scrollAmount : float = SCROLL_SPEED * delta
 	scrollPosition -= scrollAmount
 	player.position.y -= scrollAmount
+
+func createNewEnemy() -> void:
+	
+	var newEnemy : RigidBody2D = ENEMY_SCENE.instantiate()
+	newEnemy.position = player.position + Vector2(0,-200)
+	add_child(newEnemy)
+
+
+func _on_timer_timeout() -> void:
+	createNewEnemy()
+	print("timer")
+	
